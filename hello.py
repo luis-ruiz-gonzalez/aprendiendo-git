d482dcb2f5a57af8318e234980d3f2a056ddfc21
luis_ruiz_gonzalez <luis@adedelivered.com>
2022-10-20 09:06:56 +0100

    segundo commit de proyA

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@ print("Hola Mundo")
 print("14/10/2022-10:37")
 print("probando usuario")
 print("probando el autor del commit")
-print("prueba con access token")i
-print("esto es el proyecto A, ah)
+print("prueba con access token")
+print("esto es el proyecto A")
