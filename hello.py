27dfa71355c168095e423c937364814232c7616e
luis_ruiz_gonzalez <luis@adedelivered.com>
2022-10-20 08:56:32 +0100

    cambio en proyecto A

--- hello.py
+++ hello.py
@@ -2,4 +2,5 @@ print("Hola Mundo")
 print("14/10/2022-10:37")
 print("probando usuario")
 print("probando el autor del commit")
-print("prueba con access token")
+print("prueba con access token")i
+print("esto es el proyecto A, ah)
